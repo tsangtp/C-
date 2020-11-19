5d20176c15726a3e69ff2a6bd30ea525f25f7f54
tsangtakpui <tesangtakpu@yahoo.com>
2020-11-19 13:25:20 +0800

    commit

---
+++ simple_Calculation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int number1, number2;
+  cout << "Please input number1:";
+  cin >>number1;
+  cout << "Please input number2:";
+  cin >>number2;
+  cout << "number1 is " << number1 << " number2 is " << number2 <<endl;
+  cout << " number1 * number2 is " << number1 * number2 <<endl;
+  cout << " number1 / number2 is " << number1 / number2 <<endl;
+  cout << " number1 + number2 is " << number1 + number2 <<endl;
+  cout << " number1 - number2 is " << number1 - number2 <<endl;
+  cout << " number1 % number2 is " << number1 % number2 <<endl;
+
+
+  return 0;
+}
